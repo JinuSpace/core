8934ce735ea5e925c98418cdc2f098bbef78a78a
jinu <wlsdn147@naver.com>
2023-11-16 18:27:57 +0900

    Section 7-3  fix

--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -10,7 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
-
+    @Test
+    void aaaaaaa(){
+        MemoryMemberRepository memberRepository=new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "itemA", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order= orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
     @Test
     void createOrder(){
         MemoryMemberRepository memberRepository=new MemoryMemberRepository();
